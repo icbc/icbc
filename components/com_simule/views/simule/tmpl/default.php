b411c39d5e2c2137ad6784ec73512440f2b42b33
ICBC <icbc@ymail.com>
2011-08-31 17:10:05 -0300

    Criação do formulário de para execução do componente de simulação

--- components/com_simule/views/simule/tmpl/default.php
+++ components/com_simule/views/simule/tmpl/default.php
@@ -1,2 +1,24 @@
-Template<br />
-Sistemas
+<?php defined("_JEXEC") or die("Acesso Restrito") ?>
+<div id="com_simule">
+	<div class="chave">
+		<label for="campo1">Campo:</label>
+	</div>
+	<div class="campo">
+		<input type="text" class="inputbox maior" name="campo1" id="campo1" maxlenght="150" value="<?php $this->registro->campo; ?>" />
+	</div>
+	<br />
+	<div class="chave">
+		<label for="campo2">Campo:</label>
+	</div>
+	<div class="campo">
+		<input type="text" class="inputbox medio" name="campo2" id="campo2" maxlenght="150" value="<?php $this->registro->campo; ?>" />
+	</div>
+	<br />
+	<div class="chave">
+		<label for="campo3">Campo:</label>
+	</div>
+	<div class="campo">
+		<input type="text" class="inputbox menor" name="campo3" id="campo3" maxlenght="150" value="<?php $this->registro->campo; ?>" />
+	</div>
+	<br />
+</div>
--- components/com_simule/views/simule/view.html.php
+++ components/com_simule/views/simule/view.html.php
@@ -1,6 +1,12 @@
 <?php
 jimport("joomla.application.component.view");
 class SimuleFrontViewSimule extends JView{
-	
+	function display($tpl = null) {
+		$documento =& JFactory::getDocument();
+		$documento->addStyleSheet("components/com_simule/auxiliares/css/estilo.css");
+		$registro->campo = null;
+		$this->assignRef("registro",$registro);
+		parent::display($tpl);
+	}
 }
 ?>
