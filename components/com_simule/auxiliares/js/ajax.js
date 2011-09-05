4c1e6d8b253e4e7be37101e99e8015bfdf6db3d6
ICBC <icbc@ymail.com>
2011-09-05 11:13:17 -0300

    Configuração inicial do ajax do sistema

--- components/com_simule/auxiliares/js/ajax.js
+++ components/com_simule/auxiliares/js/ajax.js
@@ -1,3 +1,7 @@
-
-
-
+/* Arquivo elaborado para instanciar e configurar o Ajax do sistemas */
+var objAjax;
+if(window.XMLHttpRequest){
+	objAjax = new XMLHttpRequest();
+} else if(window.ActiveXObject) {
+	objAjax = new ActiveXObject("Microsoft.XMLHTTP");
+}
